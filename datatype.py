f75c230ef191b4033410635f816b89b9b9504b7b
Wilmara Ruiz <wilmara_andreina93@hotmail.com>
2022-10-23 08:37:45 -0500

    curso de pyton

---
+++ datatype.py
@@ -0,0 +1,42 @@
+#Tipos de datos 
+#string cadena de caracteres 
+
+print("Hello World");
+print("Hello world");
+print('Hello world');
+print("""Hello world """);
+
+print(type("hello world"));
+
+print("Bye" + " world");
+
+# Numbers
+print(30);
+print(type(30)); 
+
+#float
+print(30.5);
+print(type(30.5));
+
+#Boolean
+print(type(bool));
+
+#list se define con []
+[10,20,30,55];
+#['Hello','Bye',true,19];
+[]
+
+#tuples
+(10,20,30,35)
+()
+
+#Dictorionies
+
+print(type({
+"name":"Ryan",
+"apellido":"Ray",
+"surname":"Fazt"
+}))
+
+#Nome
+print(type(None)); 
---
+++ variables.py
@@ -0,0 +1,10 @@
+name = "fazt";
+
+print(10+101);
+print(name);
+
+x=100;
+blook="Digital Fortress";
+
+print(x);
+print(blook);
